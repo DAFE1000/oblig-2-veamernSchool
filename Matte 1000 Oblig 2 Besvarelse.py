578205f26dde046878869b22f7f01bd0ccbf835f
veamern <veamern.school@gmail.com>
2026-03-20 08:34:57 +0100

    Transfer to desktop pc

--- Matte 1000 Oblig 2 Besvarelse.py
+++ Matte 1000 Oblig 2 Besvarelse.py
@@ -4,10 +4,18 @@ import matplotlib.pyplot as plt
 def f(x):
     return (np.e**(-x/4))*np.arctan(x)
 
+def g(x):
+    return np.arctan(x)-(4/((x**2)+1))
+
 #(np.arctan(x)-(4/((x**2)+1))) = 0
+#(np.arctan(x) = (4/(x**2)+1)
+#x = np.tan((4/(x**2)+1))  ||  (np.arctan(x))*((x**2)+1) = 4
 
 x = np.linspace(-5, 20, 1000)
 plt.plot(x, f(x))
+plt.plot(x, g(x))
 plt.show()
+
+
 
 print("It barks at no one else but me like It's seen a ghost I guess it's seen the sparks flowing no one else would know hey man slow down, slow down idiot, slow down, slow down sometimes I get overcharged that's when you see sparks they ask me where the hell I'm going at a thousand feet per second hey man slow down, slow down idiot, slow down, slow down hey man slow down, slow down idiot, slow down, slow down")
